268596f57818a4a723e744f5f3029aa2eb3b1d11
KeltonSterett <keltonsterett24@gmail.com>
2023-03-09 16:24:06 -0700

    added functions to diplay the promps for password requirements

--- script.js
+++ script.js
@@ -21,15 +21,42 @@ upperCase = lowerCase.map(toUpper);
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+ 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// adding/start functions
+function generatePassword() {
+// this asks for user input
+passswordLength = prompt("Choose a number between 8 and 128");
+console.log("Password Length + passwordLength");
+
+// added if and else if statements to give prompts for criteria
+if(!passwordLength) {
+  alert("Required Value");
+
+} else if (passwordLength < 8 || passswordLength > 128) { 
+  passwordLength = prompt("Number must be between 8 and 128");
+  console.log("Password Length" + passwordLength);
+
+} else {
+confirmLower = confirm(" Would you like to include lower case letters?");
+console.log("Lower case" + confirmLower);
+confirmUpper = confirm("Would you like to include Upper case letters?");
+console.log("Upper case" + confirmUpper);
+confirmNumber = confirm("Would you like to include numbers?");
+console.log("Number" + confirmNumber);
+confirmSpecial = confirm("Would you like to include special characters?");
+console.log("Special Character" + confirmSpecial);
+
+}
+
+};
